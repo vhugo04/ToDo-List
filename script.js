9aab8e84c16acf00f7c90548857e5c0cc4f61c4b
vhugo04 <vitorhugohenrique68@gmail.com>
2025-02-24 18:44:52 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputBox = document.querySelector(".input");
+const listContainer = document.querySelector("#task-container");
+
+const error = document.querySelector("#error");
+
+function addTask() {
+    if(inputBox.value === ''){
+        error.style.visibility = "visible"
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+        error.style.visibility = "hidden";
+    }
+    inputBox.value = "";
+
+    keepData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        keepData()
+    }
+    else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        keepData()
+    }
+}, false);
+
+function keepData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function openTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+openTask();
